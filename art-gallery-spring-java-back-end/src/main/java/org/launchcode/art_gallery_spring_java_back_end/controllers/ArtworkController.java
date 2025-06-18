a96f3f88df0f295b4ad407f555b5d917b86435ce
Carolista <cjones.renaissancewoman@gmail.com>
2025-06-18 08:08:39 -0400

    updated HTML strings

--- art-gallery-spring-java-back-end/src/main/java/org/launchcode/art_gallery_spring_java_back_end/controllers/ArtworkController.java
+++ art-gallery-spring-java-back-end/src/main/java/org/launchcode/art_gallery_spring_java_back_end/controllers/ArtworkController.java
@@ -25,30 +25,36 @@ public class ArtworkController {
             Artwork artwork = artworks.get(artworkId);
             artworksList.append("<li><a href='/artworks/details/").append(artworkId).append("'>").append(artwork).append("</a></li>");
         }
-        return "<html>" +
-                "<body>" +
-                "<h2>ARTWORKS</h2>" +
-                "<ul>" +
+        return """
+                <html>
+                <body>
+                <h2>ARTWORKS</h2>
+                <ul>
+                """ +
                 artworksList +
-                "</ul>" +
-                "<p>Click <a href='/artworks/add'>here</a> to add another artwork.</p>" +
-                "</body>" +
-                "</html>";
+                """
+                </ul>
+                <p><a href='/artworks/add'>Add</a> another artwork.</p>
+                </body>
+                </html>
+                """;
     }
 
     // Corresponds to http://localhost:8080/artworks/add
     @GetMapping("/add")
     public String renderAddArtworkForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action='/artworks/add' method='POST'>" +
-                "<p>Enter the title and artist for a new work of art:</p>" +
-                "<input type='text' name='title' placeholder='Title' />" +
-                "<input type='text' name='artist' placeholder='Artist' />" +
-                "<button type='submit'>Submit</button>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return """
+                <html>
+                <body>
+                <form action='/artworks/add' method='POST'>
+                <p>Enter the title and artist for a new work of art:</p>
+                <input type='text' name='title' placeholder='Title' />
+                <input type='text' name='artist' placeholder='Artist' />
+                <button type='submit'>Submit</button>
+                </form>
+                </body>
+                </html>
+                """;
     }
 
     // Use a query parameter for dynamic results
@@ -57,27 +63,35 @@ public class ArtworkController {
     public String processAddArtworkForm(@RequestParam(value="title") String title, @RequestParam(value="artist") String artist) {
         Artwork newArtwork = new Artwork(title, artist);
         artworks.put(newArtwork.getId(), newArtwork);
-        return "<html>" +
-                "<body>" +
-                "<h3>ARTWORK ADDED</h3>" +
+        return """
+                <html>
+                <body>
+                <h3>ARTWORK ADDED</h3>
+                """ +
                 "<p>You have successfully added " + title + " to the collection.</p>" +
-                "<p><a href='/artworks/add'>Add another artwork</a> or <a href='/artworks'>view the updated list</a> of artworks.</p>" +
-                "</body>" +
-                "</html>";
+                """
+                <p><a href='/artworks/add'>Add</a> another artwork or view the <a href='/artworks'>updated list</a> of artworks.</p>
+                </body>
+                </html>
+                """;
     }
 
     // Use a path parameter for dynamic results
     // Corresponds to http://localhost:8080/artworks/details/3 (for example)
     @GetMapping("/details/{artworkId}")
     public String displayArtworkDetails(@PathVariable(value="artworkId") int artworkId) {
-        return "<html>" +
-                "<body>" +
-                "<h3>Artwork</h3>" +
+        return """
+                <html>
+                <body>
+                <h3>Artwork Details</h3>
+                """ +
                 "<p><b>ID:</b> " + artworkId + "</p>" +
                 "<p><b>Title:</b> " + artworks.get(artworkId).getTitle() + "</p>" +
                 "<p><b>Artist:</b> " + artworks.get(artworkId).getArtist() + "</p>" +
-                "</body>" +
-                "</html>";
+                """
+                </body>
+                </html>
+                """;
     }
 
 }
