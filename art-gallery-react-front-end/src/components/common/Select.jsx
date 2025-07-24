18b6ef9e6e69018a7bbc54885b4a0d50d28f1f6d
Carolista <cjones.renaissancewoman@gmail.com>
2025-07-24 13:26:43 -0400

    fixed Select layout

--- art-gallery-react-front-end/src/components/common/Select.jsx
+++ art-gallery-react-front-end/src/components/common/Select.jsx
@@ -1,11 +1,11 @@
 const Select = ({ id, label, children, handleChange }) => {
 	return (
-		<div>
+		<>
 			<label htmlFor={id}>{label}</label>
 			<select id={id} name={id} onChange={handleChange}>
 				{children}
 			</select>
-		</div>
+		</>
 	);
 };
 
